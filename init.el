6cc16bfa28fa91daf4ac48edb6484d53f1ae56f2
Filip Staffa <filip.staffa@gmail.com>
2013-12-24 00:42:21 +0100

    updated prompt from yes-no to y-n

--- init.el
+++ init.el
@@ -122,12 +122,15 @@
            (lambda () (interactive) (local-set-key (kbd "RET") 'newline-and-indent))) 
 
 
+
 (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
 (define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
 (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
 (define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
 
 (define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 
 ;;minor
