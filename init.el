88c8fcb62420ef026fc2add1817528d46f38fd9b
Filip Staffa <filip.staffa@gmail.com>
2014-04-22 21:35:07 +0200

    adds .emacs.d/lisp to load-path (instead of .emacs.d)

--- init.el
+++ init.el
@@ -83,7 +83,7 @@
 (projectile-global-mode)
 
 ;;auto-complete
-(add-to-list 'load-path "~/.emacs.d")    ; This may not be appeared if you have already added.
+(add-to-list 'load-path "~/.emacs.d/lisp")    ; This may not be appeared if you have already added.
 (require 'auto-complete-config)
 (global-auto-complete-mode)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
